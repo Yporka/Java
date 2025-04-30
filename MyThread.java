a96b67d60107932e43e6237a473b84ddfff6c5bc
Yporka <mercurial90@mail.ru>
2025-04-30 20:20:29 +0300

    Add files via upload

---
+++ MyThread.java
@@ -0,0 +1,10 @@
+public class MyThread extends Thread {
+    public MyThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Thread " + getName() + " says: Hello from thread!");
+    }
+}
