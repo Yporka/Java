fb0e298be78445da478fd5035a807c4b0875b38c
Yporka <mercurial90@mail.ru>
2025-04-30 10:01:11 +0300

    Add files via upload

---
+++ FrequencyCounter.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrequencyCounter {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Apple", "Orange", "Banana", "Apple"};
+        Map<String, Integer> frequencyMap = new HashMap<>();
+
+        for (String fruit : fruits) {
+            frequencyMap.put(fruit, frequencyMap.getOrDefault(fruit, 0) + 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
